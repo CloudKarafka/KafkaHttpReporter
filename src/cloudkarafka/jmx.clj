f3d527cdba48c8e7411c3d8c4523aa558326ca01
Magnus Landerblom <magnus@landerblom.se>
2019-01-08 12:32:45 +0100

    Flatten JMX values that are maps

--- src/cloudkarafka/jmx.clj
+++ src/cloudkarafka/jmx.clj
@@ -8,11 +8,10 @@
      (let [value (jmx/mbean mbean)]
        (for [attr attrs
              :let [v (get value (keyword attr))
-                   ;a (if group (str attr "_" (.getKeyProperty mbean group)) attr)
-                   keys (into {} (map #(vector (.getKey %) (.getValue %)) (.entrySet (.getKeyPropertyList mbean))))]]
+                   keys (into {} (map #(vector (keyword (.getKey %)) (.getValue %)) (.entrySet (.getKeyPropertyList mbean))))]]
          (if (map? v)
            (for [[k v] v]
-             (assoc keys :type (name k) :value v :attribute attr))
+             (assoc keys :key (name k) :value v :attribute attr))
            (assoc keys :value v :attribute attr))))
      (catch javax.management.InstanceNotFoundException e))))
 
@@ -21,10 +20,10 @@
   (->> (jmx/mbean-names bean)
        (mapcat #(jmx-values % attributes group))
        (remove nil?)
-       (group-by :alias) ; TODO Check what to group by here!!!
+       (group-by (keyword group))
        (map (fn [[k v]] (reduce #(assoc %1 :value (+ (:value %2) (:value %1))) v)))))
 
 (defn query [bean group attributes]
   (cond group (group-metrics bean group attributes)
         (.contains bean "*") (mapcat #(jmx-values % attributes) (jmx/mbean-names bean))
-        :else (jmx-values (jmx/as-object-name bean) attributes)))
+        :else (flatten (jmx-values (jmx/as-object-name bean) attributes))))
